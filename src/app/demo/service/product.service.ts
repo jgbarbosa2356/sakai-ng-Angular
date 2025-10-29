1983e4ede10299d9da3ff64177b8eb028bf8dbc5
João Gabriel Barbosa Costa <137813412+jgbarbosa2356@users.noreply.github.com>
2025-10-29 00:33:22 +0000

    Alterações e adição de Api no component.ts

--- src/app/demo/service/product.service.ts
+++ src/app/demo/service/product.service.ts
@@ -5,22 +5,36 @@ import { Product } from '../api/product';
 @Injectable()
 export class ProductService {
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+
+     }
+         baseURL = "https://ancient-troll-v9499vw6vvj2rxq-3000.app.github.dev/"
+
 
     getProductsSmall() {
         return this.http.get<any>('assets/demo/data/products-small.json')
             .toPromise()
             .then(res => res.data as Product[])
-            .then(data => data);
     }
 
     getProducts() {
-        return this.http.get<any>('assets/demo/data/products.json')
+        return this.http.get<any>(this.baseURL + "products" )
             .toPromise()
-            .then(res => res.data as Product[])
-            .then(data => data);
+            .then(res => {  
+                return res as Product[]})
+            
     }
 
+   addProduct(product: Product) {
+        return this.http.post<any>(this.baseURL + 'products', product)
+            .toPromise()
+            .then(res => {
+                console.log('produto adicionado', res);
+                return res as Product
+            })
+    }
+
+    
     getProductsMixed() {
         return this.http.get<any>('assets/demo/data/products-mixed.json')
             .toPromise()
